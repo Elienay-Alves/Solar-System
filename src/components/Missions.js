59896f94fefee99ff2345ccf0bd791fcb67ca50a
Elienay-Alves <elienay8486@gmail.com>
2022-02-18 19:00:49 -0300

    Requisito 8 Feito

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Titles from './Title';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
-        <p>WORK IN PROGRESS!!!</p>
+        <Titles headline="Missões" />
       </div>
     );
   }
