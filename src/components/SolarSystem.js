a4660efc7e1306867741854a0ac34d56b194d3c3
Elienay-Alves <elienay8486@gmail.com>
2022-02-18 17:07:02 -0300

    Requisito 2 Feito

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system"><p>Work in Progress</p></div>
+    );
+  }
+}
+
+export default SolarSystem;
